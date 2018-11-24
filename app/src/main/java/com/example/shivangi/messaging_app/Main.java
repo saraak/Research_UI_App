2bcf5313c25b23025d01bc68ac8f6286ecb8a869
Shivangi Agarwal <shivangiagarwal53@gmail.com>
2018-11-24 12:33:45 -0800

    Revert "Made these changes so far"
    This reverts commit f61db2351a1db321655c5487e1dcac9d8372304e.

---
+++ app/src/main/java/com/example/shivangi/messaging_app/Main.java
@@ -0,0 +1,71 @@
+package com.example.shivangi.messaging_app;
+
+
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class Main extends AppCompatActivity {
+    ImageView image;
+    Button button;
+    int clickcount;
+    long start1;
+    long end1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent (Main.this,Pop.class));
+            }
+
+        });
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        image = findViewById(R.id.imageView1);
+
+        button = findViewById(R.id.btnChangeImage);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                clickcount=clickcount+1;
+                if(clickcount%2==1)
+                {
+                    start1 = System.currentTimeMillis();
+                    image.setImageResource(R.drawable.img2);
+                    //Toast.makeText(getApplicationContext(),"Button clicked first time!", Toast.LENGTH_LONG).show();
+                }
+                else if(clickcount%2==0)
+                {
+                    end1 = System.currentTimeMillis();
+                    image.setImageResource(R.drawable.img3);
+                    //Toast.makeText(getApplicationContext(),"Button clicked count is"+clickcount, Toast.LENGTH_LONG).show();
+
+                    long time = end1 - start1;
+                    Log.d("TAG", Long.toString(time));
+                    Toast.makeText(getApplicationContext(),"Time from one click to next: "+time, Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+        });
+
+    }
+}
