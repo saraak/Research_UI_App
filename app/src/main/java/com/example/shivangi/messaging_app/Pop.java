5d73fbc91f76616daad2a2a1137908439966a642
Shivangi Agarwal <shivangiagarwal53@gmail.com>
2018-11-24 13:42:10 -0800

    voice recog

--- app/src/main/java/com/example/shivangi/messaging_app/Pop.java
+++ app/src/main/java/com/example/shivangi/messaging_app/Pop.java
@@ -1,10 +1,25 @@
 package com.example.shivangi.messaging_app;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 
-class Pop extends Activity {
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+class Pop extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -18,6 +33,71 @@ class Pop extends Activity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*.8), (int)(height*.8));
+        textView = (TextView) this.findViewById(R.id.editText2);
+        button = (Button) this.findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+                startActivityForResult(intent,200);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 200){
+            if(resultCode == RESULT_OK && data != null){
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                textView.setText(result.get(0));
+            }
+        }
     }
 
 }
+
+
+
+//public class Pop extends AppCompatActivity {
+//
+//    private Button button;
+//    private TextView textView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        textView = (TextView) this.findViewById(R.id.textView);
+//        button = (Button) this.findViewById(R.id.button);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+//                try{
+//                    startActivityForResult(intent,200);
+//                }catch (ActivityNotFoundException a){
+//                    Toast.makeText(getApplicationContext(),"Intent problem", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == 200){
+//            if(resultCode == RESULT_OK && data != null){
+//                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                textView.setText(result.get(0));
+//            }
+//        }
+//    }
+//}
