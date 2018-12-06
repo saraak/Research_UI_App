fd9bcbe67001e75adc892032f1a0453eea4c78c8
Sara Ak <sara.akhtar@github.com>
2018-12-06 11:11:16 -0800

    fix change image button

--- app/src/main/java/com/example/shivangi/messaging_app/Main.java
+++ app/src/main/java/com/example/shivangi/messaging_app/Main.java
@@ -1,23 +1,28 @@
 package com.example.shivangi.messaging_app;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main extends AppCompatActivity {
 
     private long startMilli;
     private long finishMilli;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,22 @@ public class Main extends AppCompatActivity {
 
                 startActivityForResult(
                         new Intent(Main.this, Pop.class), 0);
+            }
+        });
+
+        final ImageView imageView = findViewById(R.id.imageView1);
+        final List<Drawable> images = new ArrayList<>(3);
+        images.add(getResources().getDrawable(R.drawable.img1));
+        images.add(getResources().getDrawable(R.drawable.img2));
+        images.add(getResources().getDrawable(R.drawable.img3));
+
+        final int[] cur = {1};
+
+        Button changeImage = findViewById(R.id.btnChangeImage);
+        changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageDrawable(images.get((++cur[0])%3));
             }
         });
     }
